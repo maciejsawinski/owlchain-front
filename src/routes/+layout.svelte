e48c0a6ba40c351844411db8afcbebb083056b02
maciejsawinski <macsawinski@gmail.com>
2023-04-19 00:32:03 +0200

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+	import '@fontsource/fragment-mono';
+	import '../app.css';
+</script>
+
+<div class="navbar bg-base-100 mb-5">
+	<a href="/" class="btn btn-ghost normal-case text-xl">🦉⛓ analytics</a>
+</div>
+
+<main class="p-5">
+	<slot />
+</main>
+
+<footer class="footer footer-center p-4 bg-base-300 text-base-content mt-5">
+	<div>
+		<p>© 2023 🦉⛓</p>
+	</div>
+</footer>
+
+<style>
+	:global(body) {
+		font-family: 'Fragment Mono', monospace;
+	}
+</style>
